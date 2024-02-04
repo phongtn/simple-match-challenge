836e630b4fe6eddfac6135bbff2eab3efc7e1af1
PhongTn <Phongtn.group1@gmail.com>
2024-02-04 10:35:23 +0700

    generate two options

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,9 +4,8 @@ import {useState} from "react";
 
 export default function Component() {
     const [simpleMath, setSimpleMatch] = useState('1 / 1');
-    const [correctAnswer, setCorrectAnswer] = useState(3);
-    const [wrongAnswer, setWrongAnswer] = useState(5);
-
+    const [firstAnswer, setFirstAnswer] = useState(3);
+    const [secondAnswer, setSecondAnswer] = useState(5);
     const currentScore = '18';
 
     function generateProblem() {
@@ -18,16 +17,20 @@ export default function Component() {
             num1 = Math.floor(Math.random() * 10) + 1;
             num2 = Math.floor(Math.random() * 10) + 1;
             let correctNumber = num1 * num2;
-            // options = generateOptions(correctAnswer);
-            setSimpleMatch(num1 +' x '+ num2);
+            options = generateOptions(correctNumber, false);
+            setSimpleMatch(num1 + ' x ' + num2);
+
         } else { // Division operation
             let correctNumber = Math.floor(Math.random() * 10) + 1;
             num2 = Math.floor(Math.random() * 10) + 1;
             num1 = correctNumber * num2;
-            // options = generateOptions(correctAnswer, true);
-            setSimpleMatch(num1 +' / '+ num2);
+            options = generateOptions(correctNumber, true);
+            setSimpleMatch(num1 + ' / ' + num2);
         }
+        setFirstAnswer(options[0]);
+        setSecondAnswer(options[1])
     }
+
     function generateOptions(correctNumber: number, isDivision: boolean) {
         const option1 = correctNumber;
         let option2 = option1;
@@ -42,8 +45,6 @@ export default function Component() {
         return options;
     }
 
-
-
     return (
         <div className="flex w-full h-screen justify-center items-center">
             <div className="w-full mx-auto my-10 bg-white p-6">
@@ -57,9 +58,9 @@ export default function Component() {
                 <div className="my-6 text-center text-3xl font-bold">{simpleMath} = ?</div>
                 <div className="flex justify-center gap-4">
                     <Button className="h-12 w-24 rounded-lg bg-orange-400 text-xl font-semibold"
-                            onClick={generateProblem}>{correctAnswer}</Button>
+                            onClick={generateProblem}>{firstAnswer}</Button>
                     <Button className="h-12 w-24 rounded-lg bg-orange-400 text-xl font-semibold"
-                            onClick={generateProblem}>{wrongAnswer}</Button>
+                            onClick={generateProblem}>{secondAnswer}</Button>
                 </div>
             </div>
         </div>
