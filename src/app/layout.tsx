481317b43802605665e29b442ad8a69fc0db3895
PhongTn <Phongtn.group1@gmail.com>
2024-02-08 09:16:31 +0700

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
-import "../../styles/globals.css";
-
-
 import {Inter as FontSans} from "next/font/google";
 import React from "react";
+import {GoogleAnalytics} from "@next/third-parties/google";
+
 import {cn} from "@/lib/utils";
+import "../../styles/globals.css";
 
 const inter = Inter({subsets: ["latin"]});
 const fontSans = FontSans({subsets: ["latin"], variable: "--font-sans",})
@@ -18,16 +18,13 @@ export const metadata: Metadata = {
     description: 'Simple math game for Kids to learn multiplication table.',
     metadataBase: new URL('https://game.phongtn.com/'),
 };
-export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
-}>) {
+export default function RootLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
     return (
         <html lang="en" suppressHydrationWarning>
         <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
         {children}
         </body>
+        <GoogleAnalytics gaId="G-YX4R5K8CT3" />
         </html>
     );
 }
